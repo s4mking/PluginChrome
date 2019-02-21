59701c60e0d5705ee4f385eade23a129776188d1
Samuel Simonney <samuel@pulsar-informatique.com>
2019-02-21 15:01:58 +0100

    Ajout logout

--- script.js
+++ script.js
@@ -17,10 +17,26 @@ DataSection.forEach(element => {
    i++;
 });
 var strongText = document.querySelector('#v_card_text').innerHTML;
-var arrayStrings = strongText.split("<br>") //A voir mais a terme je pense qu'on vire les infos nom,promos qu'on met dans un deroulant à la linkedin
+var dropdownInfo = document.createElement('span');
+dropdownInfo.id="dropdown_info"
+dropdownInfo.innerHTML=strongText
+// var arrayStrings = strongText.split("<br>") //A voir mais a terme je pense qu'on vire les infos nom,promos qu'on met dans un deroulant à la linkedin
 var thenum = strongText.replace( /^\D+/g, ''); // replace all leading non-digits with nothing
 var validate = document.createElement("a")
 validate.classList.add("validation_button")
-validate.innerText="VALIDER"
+validate.innerHTML="VALIDER  &check;"
 validate.setAttribute('href', '?action=presence');
 document.querySelector('#top-wrapper').append(validate)
+
+
+var UserInformation = document.createElement('div');
+UserInformation.id="UserInfo";
+UserInformation.innerHTML="Mes Infos &#9662; <a href='?action=logout'>&#128275</a>"
+document.querySelector("#v_card_photo").append(UserInformation)
+document.querySelector("#v_card_photo").append(dropdownInfo)
+check=true
+UserInformation.addEventListener('click',function(){
+    check==false?dropdownInfo.style.display="none":dropdownInfo.style.display="block"
+    check==false?this.style.color="black":this.style.color="grey"
+    check==true?check=false:check=true
+})
